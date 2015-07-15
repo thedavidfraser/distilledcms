8ecccd31efe9b4d08cdfb8bddbb6e1ca79d9acaf
David Fraser <david.fraser@digitaslbi.com>
2015-07-15 15:24:08 +0100

    JS and fonts structure. HTML in root. Gulp processes updated to support structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	rename = require('gulp-rename'),
-	minifycss = require('gulp-minify-css');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    minifycss = require('gulp-minify-css'),
+
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify');
 
 
 // SCSS
@@ -14,13 +19,71 @@ gulp.task('sass', function () {
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('./dist/css'))
+    .pipe(notify({ message: 'SASS task complete' }));
+});
+
+
+// JS
+gulp.task('js', function() {
+  return gulp.src('./dev/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(notify({ message: 'JS task complete' }));
+});
+
+
+// Copy JS assets
+gulp.task('copyJsAssets', function() {
+
+  var src = [
+        './bower_components/jquery/dist/jquery.min.js'
+      ];
+
+  return gulp.src(src)
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(notify({ message: 'Copy JS assets task complete' }));
+});
+
+// Copy HTML assets
+gulp.task('copyHtmlAssets', function() {
+
+  var src = [
+        './dev/start.html'
+      ];
+
+  return gulp.src(src)
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify({ message: 'Copy HTML assets task complete' }));
+});
+
+// Copy fonts
+gulp.task('copyFontAssets', function() {
+
+  var src = [
+        './dev/fonts/fonts/icomoon.eot',
+        './dev/fonts/fonts/icomoon.svg',
+        './dev/fonts/fonts/icomoon.ttf',
+        './dev/fonts/fonts/icomoon.woff'
+      ];
+
+  return gulp.src(src)
+    .pipe(gulp.dest('./dist/fonts'))
+    .pipe(notify({ message: 'Copy fonts task complete' }));
 });
 
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch('./dev/scss/**/*.scss', ['sass']);
+  gulp.watch('./dev/js/**/*.js', ['js']);
+  gulp.watch('./dev/**/*.html', ['copyHtmlAssets']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'sass']);
+gulp.task('default', ['watch', 'sass', 'js', 'copyJsAssets', 'copyHtmlAssets', 'copyFontAssets']);
+
