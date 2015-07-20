9b0f04a1b85c065c9abb18d14c4ede4e8df4850e
David Fraser <david.fraser@digitaslbi.com>
2015-07-20 14:24:48 +0100

    PA Me stying improvements. Smaller finer font for headings

--- dev/js/scriptFooter.js
+++ dev/js/scriptFooter.js
@@ -4,7 +4,7 @@ module.exports = function() {
 
   var appendFooter = function(){
 
-    var $footer = $('<div class="footer"><h2>PA Me <span>footer</span></h2><p>&copy; 2015 PA Me Ltd</p></h2></div>');
+    var $footer = $('<div class="footer"><h2>PA Me <span>footer</span></h2><p><a href="http://www.pame.uk">Home</a> | <a href="/about">About</a> | <a href="mailto:hello@pame.uk">hello@pame.uk</a><p>&copy; 2015 PA Me Ltd</p></h2></div>');
     $('body').append($footer);
 
   };
