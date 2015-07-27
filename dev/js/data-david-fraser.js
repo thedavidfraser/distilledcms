0d58496c7c50cb44d1b107709757d0357904160e
David Fraser <david.fraser@digitaslbi.com>
2015-07-27 14:02:58 +0100

    David Fraser specific files, data and styles only

---
+++ dev/js/data-david-fraser.js
@@ -0,0 +1,61 @@
+module.exports = {
+    "content" : [
+      "David Fraser",
+      [
+        "Lead front-end engineer",
+
+        "I’m a front-end developer living in London. I've made websites for Virgin Atlantic, BlackBerry, Premier League, ATP Tennis and The Co-operative. I’m passionate about quality code and design and excited about new technologies disrupting how we use the internet.",
+
+        "<a href=\"mailto:hi@davidfraser.co.uk\">Email hi@davidfraser.co.uk</a>"
+
+      ],
+      [
+        "Projects I’ve played a key role in delivering",
+
+        [
+          "Virgin Atlantic",
+          "Adaptive desktop and mobile websites for the famous airline, including fight selection and booking flow.",
+          "Best Airline website at Travelolgy Awards 2014 voted by The Sun readers",
+          "David Fraser worked on Virgin Atlantic whilst at Digitaslbi LBi",
+          "Visit Virgin Atlantic"
+        ],
+        [
+          "DigitasLBi Marketing Cloud",
+          "Browser based applications that present big data in an easy to digest format allowing insights into how high-street brands compare on social media and wider web.",
+          "David Fraser worked on Marketing Cloud whilst at DigitasLBi",
+          "Learn more about DigitasLBi Marketing Cloud"
+        ],
+        [
+          "Appsme",
+          "Browser based application and iOS “emulator” that allows anyone to make an app in minutes using their Facebook account or by answering a few questions. 100,000 apps have been built by this and similar tools by Mippin.",
+          "David Fraser worked on Appsme whilst at Mippin",
+          "Create an app with Appsme"
+        ],
+        [
+          "Mippin",
+          "A suite of “App builders” that allow anyone to make a cross-platform mobile app and mobile optimised website based on their RSS feeds. Some apps were HTML5 based but were indistinguishable from native equivilents.",
+          "BlackBerry Achievement Awards Finalist 2013, AOP Digital Publishing Awards Finalist 2012, Most Effective Mobile Publishing Solution 2011, Nokia Innovation Excellence Award 2010",
+          "Learn more about Mippin"
+        ]
+      ],
+      [
+        "In development",
+        [
+          "Distilled",
+          "A personal project available on Github. Rethinking how we build websites. Looking into ways of improving content and enforcing best-practices whilst making websites simpler to manage and maintain.",
+          "Distilled on Github"
+        ]
+      ],
+      [
+        "Non development",
+        "When I’m not developing I like brand and user experience design. Examples include working with Mippin’s Creative Director on the Appsme brand and user journey. Brand and webpage for PA Me, webpage created with Distilled.",
+        "Visit Appsme",
+        "Visit PA Me"
+      ],
+      [
+        "Get in touch",
+        "If you’d like to learn more then please send me an email",
+        "<a href=\"mailto:hi@davidfraser.co.uk\">Email hi@davidfraser.co.uk</a>"
+      ]
+    ]
+};
