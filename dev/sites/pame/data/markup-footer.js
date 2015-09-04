8cd15188916f9268967cfbf852c22f927a5ca770
David Fraser <david.fraser@digitaslbi.com>
2015-09-04 12:51:30 +0100

    PA Me legal copy added

--- dev/sites/pame/data/markup-footer.js
+++ dev/sites/pame/data/markup-footer.js
@@ -7,6 +7,6 @@ module.exports = function() {
 
   "use strict";
 
-  return '<div class="footer">\n\t<h2>PA Me <span>footer</span></h2>\n\t<p><a href="http://www.pame.uk">Home</a> | <a href="/about">About</a> | <a href="mailto:hello@pame.uk">hello@pame.uk</a>\n\t<p>&copy; 2015 PA Me Ltd</p>\n</div>';
+  return '<div class="footer">\n\t<h2>PA Me <span>footer</span></h2>\n\t<p><a href="http://www.pame.uk">Home</a> | <a href="/about">About</a> | <a href="mailto:hello@pame.uk">hello@pame.uk</a>\n\t<p>&copy; PA Me Ltd 2015. Registered in England No. 09575092. 6 SW16 5UZ</p>\n</div>';
 
 };
